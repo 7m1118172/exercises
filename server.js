ac0f4e11e1b5da4f4e736fbb1a0415ead435c706
7m1118172 <hussainbanijamra@gmail.com>
2026-04-02 16:33:50 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// السماح بالوصول لكل الملفات (الخط، الصور، الخ)
+// السماح بالوصول لكل الملفات الثابتة (الخط، الصور، الخ)
 app.use(express.static(__dirname));
 
-// عند طلب أي رابط، أظهر ملف الـ index.html
+// أي طلب آخر، أظهر ملف الـ index.html (مهم لمسار /admin)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// تشغيل السيرفر على جميع العناوين ليتوافق مع Render
+// تشغيل السيرفر على جميع العناوين
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
